fda3a774be552758b5211bb2b004b7fa45a350ef
Vignesh Kathirkamar <59691368+VigneshKathirkamar@users.noreply.github.com>
2022-01-31 10:59:26 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,28 @@
+from distutils.log import debug
+from flask import Flask, render_template, Response
+import cv2
+
+app = Flask(__name__)
+camera = cv2.VideoCapture(0)
+
+def generate_frame():
+    while True:
+        ret, img = camera.read()
+        if not ret:
+            break
+        else:
+            success, buffer = cv2.imencode('.jpg',img)
+            img = buffer.tobytes()
+        yield(b'--frame\r\n'
+                    b'Content-Type: image/jpeg\r\n\r\n'+ img +b'\r\n')
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/video')
+def video():
+    return Response(generate_frame(),mimetype='multipart/x-mixed-replace; boundary=frame')
+
+if __name__ == '__main__':
+    app.run(debug=True)
